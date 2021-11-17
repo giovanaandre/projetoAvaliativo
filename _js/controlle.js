121145f74ad5d77688d38b9bfb5aa1a157f78052
GIOVANA ANDRE ARAUJO <giovana.andre@alunoescolassociais.org.br>
2021-11-17 14:46:50 -0300

    organiza arquivos do projeto

---
+++ _js/controlle.js
@@ -0,0 +1,24 @@
+/*##############
+Inicio: Função de verificação dos campos do formulário
+*/
+function {
+	var nome = document.getElementById('nome').value;
+	var email = document.getElementById('email').value;
+	var dtnasc = document.getElementById('dtnasc').value;
+	var fone = document.getElementById('fone').value;
+	var cpf = document.getElementById('cpf').value;
+	var senha = document.getElementById('senha').value;
+	var PJ = document.getElementById('pessoajuridica').value;
+	var PF = document.getElementById('pessoafisica').value;
+	var genero = document.getElementById('genero').value;
+
+
+	// || !dtnasc || !fone || !cpf || !senha || !PF || !PJ || !genero
+	if(!nome || !email){
+		alert("Campos não preenchidos, favor preenche-los");
+	} else{
+		alert("Campos preenchidos com sucesso");
+	}
+}
+/*##############
+Final: Função de verificação dos campos do formulário
